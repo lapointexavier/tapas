2c5a9cae4311a56da89450d026c2179dcc7c2197
Guillaume Laforge <guillaume.laforge.3d@gmail.com>
2012-06-13 17:00:53 -0400

    Forget to add the modified python file!

---
+++ tapas_linux64/tapas.py
@@ -0,0 +1,32 @@
+from tapas import Tapas
+from tapas import Restaurant
+
+class MontrealSiTdsMeeting(object):
+    def __init__(self, date, restaurant):
+        self.owners = ('Alan', 'Miquel')
+        self.__gathering = date
+        self.__restaurant = restaurant
+
+    def gotTapas(self):
+        goodItems = filter(lambda item: isinstance(item, Tapas), self.__restaurant.menu)
+        if len(goodItems) > 0:
+            return True
+        return False
+
+    def available(self, date=None):
+        if date or self.__gathering:
+            return True
+        return False
+
+    def ImIn(self):
+        if self.available() and self.gotTapas():
+            return True
+        return False
+
+def main():
+    restaurant = Restaurant("Club Espagnol du Quebec", [Tapas(t) for t in ['pulpo', 'tortilla de patatas', 'paella valenciana']])
+    spanishLunch = MontrealSiTdsMeeting('06-14-12', restaurant)
+    print spanishLunch.ImIn()
+
+if __name__ == '__main__':
+    main()
